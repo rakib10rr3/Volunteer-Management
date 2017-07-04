12a34ae3fd22282a5fbaf5b363bd7c0b4c7be511
imranctg16 <imranhossain16.ctg@gmail.com>
2017-07-05 02:09:36 +0600

    group_details group_search

---
+++ group_details.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Me
+ * Date: 7/4/2017
+ * Time: 11:35 PM
+ */
+include "basic_structure/header.php";
+include "basic_structure/navbar.php";
+//include "db_util.php";-->included in the header file
+?>
+
+<?php
+
+
+
+/***
+ * Functions of this page
+ *
+ * you will find a end tag like this when functions end
+ * ----
+ */
+
+function get_this_group($grp_id)
+{
+    $db=new db_util();
+    $query_string="SELECT * FROM vm_group WHERE v_group_id=$grp_id";
+    $result=$db->query($query_string);
+    return $result;
+}
+function get_group_member_list($grp_id)
+{
+    $db=new db_util();
+    $query_string="SELECT * FROM vm_member_list WHERE vm_group_id=$grp_id";
+    $result=$db->query($query_string);
+    return $result;
+}
+
+function show_group_info($v_group_id,$v_group_name,$v_group_place,$v_group_description,$v_group_services,$v_group_member_number,$v_group_leader_id)
+{
+
+    echo '<div class="w3-container"> 
+            <div class="w3-card-4" style="width:50%";> 
+            <header class="w3-container w3-blue">
+               <h1>'. $v_group_name.'</h1>
+            </header>
+            <div class="w3-container">
+                <ul class="list-group">
+                    <li class="list-group-item">.Place: '.$v_group_place .'</li>
+                    <li class="list-group-item">Description :'.$v_group_description .'</li>
+                    <li class="list-group-item">leader_id: '.$v_group_leader_id .'</li>
+                    <li class="list-group-item">Total Member :'.$v_group_member_number .'</li>
+                    <li class="list-group-item">Offered Services :'.$v_group_services .'</li>
+                </ul>
+            </div>
+        </div>
+    </div>';
+}
+function show_member_list($v_group_id)
+{
+
+    echo $v_group_id;
+    $result=get_group_member_list($v_group_id);
+
+    if($result)
+    {
+        while ($row=mysqli_fetch_assoc($result))
+        {
+            $vm_member_id=$row['vm_member_list_id'];
+            $vm_member_name=$row['vm_member_name'];
+            $vm_member_email=$row['vm_member_email'];
+            $vm_member_phone=$row['vm_member_phone'];
+            $vm_member_type=$row['vm_member_type'];
+
+            echo '
+                <div class="w3-container">
+                    <ul class="list-group">
+                        <li class="list-group-item">Member_id:'. $vm_member_id.'</li>
+                        <li class="list-group-item">Name : '.$vm_member_name .'</li>
+                        <li class="list-group-item">Member_email: '.$vm_member_email.' </li>
+                        <li class="list-group-item">Member_email :'. $vm_member_phone .'</li>
+                        <li class="list-group-item">Member_type :'. $vm_member_type .'</li>
+                    </ul>
+                </div>';
+        }
+    }
+    else
+    {
+        echo "Sorry couldnt find this group_member list ";
+    }
+}
+
+
+/***
+ * end of function php tags
+ */
+?>
+
+
+
+
+<?php
+  if(isset($_GET['group_id'])) {
+
+      $result=get_this_group($_GET['group_id']);
+
+      if($result)
+      {
+          while ($row=mysqli_fetch_assoc($result))
+          {
+              $v_group_id=$row['v_group_id'];
+              $v_group_name=$row['v_group_name'];
+              $v_group_place=$row['v_group_place'];
+              $v_group_description=$row['v_group_description'];
+              $v_group_services=$row['v_group_services'];
+              $v_group_member_number=$row['v_group_member_number'];
+              $v_group_leader_id=$row['v_group_leader_id'];
+              show_group_info($v_group_id,$v_group_name,$v_group_place,$v_group_description,$v_group_services,$v_group_member_number,$v_group_leader_id);
+              show_member_list($v_group_id);
+          }
+      }
+      else
+      {
+          echo "Sorry couldnt find this group ";
+      }
+  }
+  else
+  {
+      echo "<h3 class='text-danger'>What are You doing in this page :/ </h3> ";
+  }
+
+?>
+
+
+
+<?php
+include "basic_structure/footer.php"
+?>
+
+
+
+
---
+++ group_search.php
@@ -0,0 +1,120 @@
+<?php
+
+include "basic_structure/header.php";
+include "basic_structure/navbar.php";
+//include "db_util.php";-->included in the header file
+
+
+?>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Me
+ * Date: 7/4/2017
+ * Time: 7:04 PM
+ * ----
+ */
+//error variable
+$place_error = "";
+
+if (isset($_POST['Search'])) {
+    $place = $_POST['place'];
+
+    if (empty($place)) {
+        $place_error = "Field Cannot be empty ! ";
+
+    }
+    //echo "user is searching for the place ".$place;
+    if (empty($place_error)) {
+        search_the_place($place);
+
+    }
+
+}
+?>
+
+
+
+<?php
+
+/***
+ * ei page er function gula ekhane likhtesi
+ */
+function search_the_place($place)
+{
+    $value = "";
+    $db = new db_util();
+    $query_string = "SELECT * FROM vm_group where v_group_place LIKE '%$place%' ";
+    $result = $db->query($query_string);
+    if (mysqli_num_rows($result) > 0) {
+        echo "<h2 class='alert-success '>Total " . mysqli_num_rows($result) . " Results found !</h2>";
+        show_all_the_group($result);
+    } else {
+        echo "<h2 class='text-center text-danger '>Sorry ! Couldnt find any group </h2>";
+        echo "<a href=''>Create a Group </a>";
+    }
+
+
+}
+function show_all_the_group($result)
+{
+
+?>
+
+<div class="col-md-12">
+    <table class=" table table-bordered table-responsive table-hover">
+        <thead>
+        <tr>
+            <th> Name</th>
+            <th> Place</th>
+            <th> description</th>
+            <th> Member_number</th>
+            <th> Leader</th>
+            <th> </th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        while ($row = mysqli_fetch_assoc($result)) {
+
+            $group_name = $row['v_group_name'];
+            $group_id = $row['v_group_id'];
+            $group_place = $row['v_group_place'];
+            $group_desc = $row['v_group_description'];
+            $group_leader_name = $row['v_group_leader_id'];
+            $group_member_number = $row['v_group_member_number'];
+            echo "<tr>";
+            echo "<td>{$group_name}</td>";
+            echo "<td>{$group_place}</td>";
+            echo "<td>{$group_desc}</td>";
+            echo "<td>{$group_leader_name} </td>";
+            echo "<td>{$group_member_number} </td>";
+            echo "<td> <a class='navbar-link' href='group_details.php?group_id=$group_id'>View Details </a> </td>";
+            echo "</tr>";
+        }
+        // exit();
+        ?>
+        </tbody>
+    </table>
+
+    <?php
+    }
+    ?>
+
+
+    <form method="post" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+
+        <?php if (!empty($place_error)) {
+            echo "<h3  class='text-center text-danger'>$place_error</h3>";
+        }
+        ?>
+        <label for="place"> Search With a Place </label>
+        <input type="text" name="place">
+        <br>
+
+        <input type="submit" value="Search" name="Search">
+    </form>
+<?php
+include "basic_structure/footer.php";
+?>
